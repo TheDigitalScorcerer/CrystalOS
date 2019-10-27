50645e7d6007fe4487a67475b0340509da15088e
TheDigitalScorcerer <31848971+TheDigitalScorcerer@users.noreply.github.com>
2019-10-27 20:22:28 +0800

    Worked on CrystalOS in ASM, made a folder for the functions.

---
+++ asm/print.asm
@@ -0,0 +1,19 @@
+printstr:				; Routine: output string in SI to screen
+    pusha
+	mov ah, 0Eh			; int 10h 'print char' function
+.repeat:
+	lodsb				; Get character from string
+	cmp al, 0
+	je .done			; If char is zero, end of string
+	int 10h				; Otherwise, print it
+	jmp .repeat
+.done:
+    popa
+	ret
+
+printchar:
+    pusha
+    mov ah, 0x0e
+    int 0x10
+    popa
+    ret
--- boot.asm
+++ boot.asm
@@ -7,40 +7,51 @@ start:
     call printstr
     mov si, EXIT_MSG
     call printstr
+
+    mov [BOOT_DRIVE], dl
+    mov bp, 0x8000
+    mov sp, bp              ; Move the stack out of the way
+
+    mov bx, 0x9000
+    mov dh, 5
+    mov dl, [BOOT_DRIVE]    ; Load 5 sectors of the boot disk
+    call disk_load
+
+    mov si, [0x9000]
+    call printstr
+
+    mov si, [0x9000 + 512]
+    call printstr
+
+    mov si, HYPERSPACE_A
+    call printstr
+
+    ; PREPARE FOR 32-BITS!
+
+    mov si, HYPERSPACE_B
+    call printstr
+
     jmp exit_os
 
 ; -------------------------------- ;
 ;           My Functions           ;
 ; -------------------------------- ;
 
-printstr:				; Routine: output string in SI to screen
-    pusha
-	mov ah, 0Eh			; int 10h 'print char' function
-.repeat:
-	lodsb				; Get character from string
-	cmp al, 0
-	je .done			; If char is zero, end of string
-	int 10h				; Otherwise, print it
-	jmp .repeat
-.done:
-    popa
-	ret
+%include "asm/print.asm"
+%include "asm/disk.asm"
 
-printchar:
-    pusha
-    mov ah, 0x0e
-    int 0x10
-    popa
-    ret
+BOOTING_MSG     db 'Hello, World! CrystalOS is booting...', 0x0D, 0x0A, 0x00
+EXIT_MSG        db 'Goodbye! Shutting down now...', 0x0D, 0x0A, 0x00
+HYPERSPACE_A    db 'You are in 16-bit Real Mode. Preparing to make the jump to 32-bit Protected Mode...', 0x0D, 0x0A, 0x00
+HYPERSPACE_B    db 'Making the jump to 32-bit! See you soon!', 0x0D, 0x0A, 0x00
 
-
-BOOTING_MSG db 'Hello, World! CrystalOS is booting...', 0x0D, 0x0A, 0x00
-EXIT_MSG    db 'Goodbye! Shutting down now...', 0x0D, 0x0A, 0x00
-
-DISK_ERROR  db 'Error reading disk!', 0x0D, 0x0A, 0x00
+BOOT_DRIVE: db 0
 
 exit_os:
     hlt
 
 	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
 	dw 0xAA55				; The standard PC boot signature
+
+    times  256 dw 0x6500
+    times  256 dw 0x6A00
